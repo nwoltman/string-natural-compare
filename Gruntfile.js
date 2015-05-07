ab1eb60cd8b8d623642bc015efbd052ab456e9b7
Nathan Woltman <nwoltman@outlook.com>
2015-05-07 01:13:02 -0400

    Set up repo
    Mostly copied things over from woollybogger/pro-array

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jsonlint: {
+      all: {
+        src: ['*.json', '.jshintrc']
+      }
+    },
+
+    jshint: {
+      all: {
+        src: ['*.js', 'test/*.js'],
+        options: {
+          jshintrc: true
+        }
+      }
+    },
+
+    jscs: {
+      all: ['*.js', 'test/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+
+    mochacov: {
+      test: {
+        options: {
+          reporter: 'spec'
+        }
+      },
+      coverage: {
+        options: {
+          reporter: 'html-cov',
+          quiet: true,
+          output: 'coverage/coverage.html'
+        }
+      },
+      testAndCoverage: {
+        options: {
+          coveralls: true
+        }
+      },
+      options: {
+        files: 'test/*.js'
+      }
+    }
+  });
+
+  // Load the Grunt plugins
+  grunt.loadNpmTasks('grunt-jsonlint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-mocha-cov');
+
+  // Register tasks
+  grunt.registerTask('lint', ['jsonlint', 'jshint', 'jscs']);
+  grunt.registerTask('test', ['mochacov:test'].concat(process.env.CI ? ['mochacov:testAndCoverage'] : []));
+  grunt.registerTask('coverage', ['mochacov:coverage']);
+  grunt.registerTask('default', ['lint', 'test']);
+
+  grunt.registerTask('changelog', 'Add the changes since the last release to the changelog', function() {
+    var done = this.async();
+
+    var pkg = grunt.config.data.pkg;
+    var repoUrl = pkg.repository.url;
+    var command =
+      'git --no-pager log v' + pkg.version + '... --pretty=format:"+ %s ([view](' + repoUrl + '/commit/%H))"';
+
+    require('child_process').exec(command, function(error, stdout) {
+      if (error) {
+        grunt.log.error('There was an error reading the git log output.');
+        grunt.fail.fatal(error);
+      }
+
+      var fs = require('fs');
+      var code = fs.readFileSync('index.js', {encoding: 'utf8'});
+      var curVersion = /@version (\d+\.\d+\.\d+)/.exec(code)[1];
+      var date = new Date().toISOString().slice(0, 10);
+      var versionHeader = '## ' + curVersion + ' (' + date + ')\n';
+      var changelog = fs.readFileSync('CHANGELOG.md', {encoding: 'utf8'});
+
+      if (changelog.indexOf(versionHeader, 13) >= 0) {
+        grunt.log.error('Changelog already updated.');
+        return done();
+      }
+
+      // Filter out messages that don't need to be in the changelog
+      var changes = stdout.replace(/^\+ (?:Update|Merge).*[\r\n]*/gm, '');
+
+      changelog = '# CHANGELOG\n\n' +
+                  versionHeader + changes + '\n' +
+                  changelog.replace(/^# CHANGELOG\s+/, '\n'); // Remove the current header
+
+      fs.writeFile('CHANGELOG.md', changelog, done);
+      grunt.log.ok('Added changes to the changelog.');
+    });
+  });
+};
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # node-natural-compare
-A fast natural string comparison algorithm
+
+A fast natural string comparison algorithm for Node
+
+[![NPM Version](https://img.shields.io/npm/v/node-natural-compare.svg)](https://www.npmjs.com/package/node-natural-compare)
+[![Build Status](https://travis-ci.org/woollybogger/node-natural-compare.svg?branch=master)](https://travis-ci.org/woollybogger/node-natural-compare)
+[![Coverage Status](https://coveralls.io/repos/woollybogger/node-natural-compare/badge.svg?branch=master)](https://coveralls.io/r/woollybogger/node-natural-compare?branch=master)
+[![Dependency Status](https://david-dm.org/woollybogger/node-natural-compare.svg)](https://david-dm.org/woollybogger/node-natural-compare)
+[![devDependency Status](https://david-dm.org/woollybogger/node-natural-compare/dev-status.svg)](https://david-dm.org/woollybogger/node-natural-compare#info=devDependencies)
+
+
+## Installation
+
+```sh
+npm install node-natural-compare --save
+```
+
+## Usage
+
+```js
+var naturalCompare = require('node-natural-compare');
+
+// Simple case sensitive example
+var a = ['z1.doc', 'z10.doc', 'z17.doc', 'z2.doc', 'z23.doc', 'z3.doc'];
+a.sort(naturalCompare);
+// -> ['z1.doc', 'z2.doc', 'z3.doc', 'z10.doc', 'z17.doc', 'z23.doc']
+
+// Use wrapper function for case insensitivity
+a.sort(function(a, b){
+  return naturalCompare(a.toLowerCase(), b.toLowerCase());
+});
+
+// In most cases we want to sort an array of objects
+var a = [
+  {street: '350 5th Ave', room: 'A-1021'},
+  {street: '350 5th Ave', room: 'A-21046-b'}
+];
+
+// Sort by street, then by room
+a.sort(function(a, b) {
+  return naturalCompare(a.street, b.street) || naturalCompare(a.room, b.room);
+});
+
+// When text transformation is needed (e.g. toLowerCase()), it is best for
+// performance to keep the transformed key in that object. That way, the
+// text transformation will not be needed for every comparison when sorting.
+var a = [
+  {make: 'Audi', model: 'A6'},
+  {make: 'Kia',  model: 'Rio'}
+];
+
+// Sort by make, then by model
+a = a.map(function(car) {
+  car.sort_key = (car.make + ' ' + car.model).toLowerCase();
+});
+a.sort(function(a, b) {
+  return naturalCompare(a.sort_key, b.sort_key);
+});
+```
+
+Requires [browserify](https://www.npmjs.com/package/browserify) to work in the browser.
