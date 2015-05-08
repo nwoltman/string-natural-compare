ca7d55174e23f2d14a1f817243ff949d44b5603f
Nathan Woltman <nwoltman@outlook.com>
2015-05-08 18:30:06 -0400

    Remove grunt changelog generator task
    I'm pretty sure I'll never need it.
    
    [ci skip]

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,42 +82,4 @@ module.exports = function(grunt) {
   grunt.registerTask('coverage', ['mochacov:coverage']);
   grunt.registerTask('build', ['copy', 'uglify']);
   grunt.registerTask('default', ['lint', 'test', 'build']);
-
-  grunt.registerTask('changelog', 'Add the changes since the last release to the changelog', function() {
-    var done = this.async();
-
-    var pkg = grunt.config.data.pkg;
-    var repoUrl = pkg.repository.url;
-    var command =
-      'git --no-pager log v' + pkg.version + '... --pretty=format:"+ %s ([view](' + repoUrl + '/commit/%H))"';
-
-    require('child_process').exec(command, function(error, stdout) {
-      if (error) {
-        grunt.log.error('There was an error reading the git log output.');
-        grunt.fail.fatal(error);
-      }
-
-      var fs = require('fs');
-      var code = fs.readFileSync('index.js', {encoding: 'utf8'});
-      var curVersion = /@version (\d+\.\d+\.\d+)/.exec(code)[1];
-      var date = new Date().toISOString().slice(0, 10);
-      var versionHeader = '## ' + curVersion + ' (' + date + ')\n';
-      var changelog = fs.readFileSync('CHANGELOG.md', {encoding: 'utf8'});
-
-      if (changelog.indexOf(versionHeader, 13) >= 0) {
-        grunt.log.error('Changelog already updated.');
-        return done();
-      }
-
-      // Filter out messages that don't need to be in the changelog
-      var changes = stdout.replace(/^\+ (?:Update|Merge).*[\r\n]*/gm, '');
-
-      changelog = '# CHANGELOG\n\n' +
-                  versionHeader + changes + '\n' +
-                  changelog.replace(/^# CHANGELOG\s+/, '\n'); // Remove the current header
-
-      fs.writeFile('CHANGELOG.md', changelog, done);
-      grunt.log.ok('Added changes to the changelog.');
-    });
-  });
 };
