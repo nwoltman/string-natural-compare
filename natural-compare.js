ce22196ef2b00559678ef2ed2ea6b228b6213d65
leehao <lee_haoo@163.com>
2018-10-16 12:38:51 +0800

    Fix sort when there are many leading 0s (#4)
    Fixes #3

--- natural-compare.js
+++ natural-compare.js
@@ -77,6 +77,14 @@ function naturalCompare(a, b) {
     ++bIndex;
   }
 
+  if (aIndex >= lengthA && bIndex < lengthB && lengthA >= lengthB) {
+    return -1;
+  }
+
+  if (bIndex >= lengthB && aIndex < lengthA && lengthB >= lengthA) {
+    return 1;
+  }
+
   return lengthA - lengthB;
 }
 
